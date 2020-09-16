e30b61ea5c16934921e0cf5bd133c93434e7f8ae
Simon Babay <simon@babay.fr>
2020-09-16 18:44:24 +0200

    Add UVue

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,11 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+export default () => {
+  return new Vuex.Store({
+    state: () => ({}),
+    mutations: {},
+    actions: {},
+    modules: {}
+  });
+};
